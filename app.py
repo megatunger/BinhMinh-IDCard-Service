68bfc1856cd77abdb2ae33d9011fb988f80efbc0
Tung <megatunger@gmail.com>
2020-08-08 16:07:38 +0700

    update new card style!

--- app.py
+++ app.py
@@ -23,11 +23,9 @@ def download(filename):
 def main():
     id = request.args.get('id', ' ')
     name = request.args.get('name', ' ')
-    birthday = request.args.get('birthday', ' ')
     school = request.args.get('school', ' ')
-    phone = request.args.get('phone', ' ')
     id_encrypt = request.args.get('id_encrypt', ' ')
-    return process(id, name, birthday, school, phone, id_encrypt)
+    return process(id, name, school, id_encrypt)
 
 @app.route('/', methods = ['DELETE'])
 def clearCache():
@@ -40,33 +38,27 @@ def clearCache():
     except:
         return "file not found."
 
-def process(id, name, birthday, school, phone, id_encrypt):
+def process(id, name, school, id_encrypt):
     # Set params
     string_limit = 21
     id = id[:string_limit]
     name = name[:string_limit]
-    birthday = birthday[:string_limit]
     school = school[:string_limit]
-    phone = phone[:string_limit]
     id_encrypt = id_encrypt
     # opening image
-    img = Image.open("plain.png")
-    draw = ImageDraw.Draw(img)
+    img_front = Image.open("Template/Front.png")
+    img_back = Image.open("Template/Back.png")
+    draw = ImageDraw.Draw(img_front)
 
     # opening font files
-    font_regular = ImageFont.truetype("fonts/Montserrat-Regular.otf", 64)
-    font_bold = ImageFont.truetype("fonts/Montserrat-SemiBold.otf", 64)
+    font_regular = ImageFont.truetype("fonts/Montserrat-Regular.otf", 56)
+    font_bold = ImageFont.truetype("fonts/Montserrat-SemiBold.otf", 56)
 
     # Writing text at specific positions and specific color.
-    left_margin = 475
-    start_text = 412.5
-    line_spacing = 132
 
-    draw.text((left_margin, start_text), id, (74, 75, 76), font=font_bold)
-    draw.text((left_margin, start_text + line_spacing), name, (74, 75, 76), font=font_regular)
-    draw.text((left_margin, start_text + line_spacing*2), birthday, (74, 75, 76), font=font_regular)
-    draw.text((left_margin, start_text + line_spacing*3), school, (74, 75, 76), font=font_regular)
-    draw.text((left_margin, start_text + line_spacing*4), phone, (74, 75, 76), font=font_regular)
+    draw.text((193, 403), id, (22, 28, 42), font=font_bold)
+    draw.text((193, 711), name, (22, 28, 42), font=font_bold)
+    draw.text((193, 1010), school, (22, 28, 42), font=font_bold)
 
     # Create qr image
     qr = pyqrcode.create('{"login_token":"'+id_encrypt+'","otp": "offline_card"}')
@@ -74,35 +66,44 @@ def process(id, name, birthday, school, phone, id_encrypt):
     filename_qr_png = 'output/qr/' + id + '.png'
     os.makedirs(os.path.dirname(filename_qr_png), exist_ok=True)
 
-    qr.png(filename_qr_png, scale=8)
+    qr.png(filename_qr_png, scale=12)
 
     # QR image is superimposed on the ID-card image with itself as a filter
     qr_img = Image.open(filename_qr_png)
-    img.paste(qr_img, (1334, 383))
+    img_front.paste(qr_img, (1215, 345))
 
     #Save to disk
 
-    filename_png = 'output/png/' + id + '.png'
-    os.makedirs(os.path.dirname(filename_png), exist_ok=True)
+    filename_front_png = 'output/png/' + id + '_front.png'
+    filename_back_png = 'output/png/' + id + '_back.png'
+    os.makedirs(os.path.dirname(filename_front_png), exist_ok=True)
+    os.makedirs(os.path.dirname(filename_back_png), exist_ok=True)
 
-    img.save(filename_png)
+    img_front.save(filename_front_png)
+    img_front.save(filename_back_png)
 
     # To convert to pdf the image is temporarily stored as jpeg
-    rgb_img = img.convert('RGB')
-    rgb_img.save("temp.jpg")
-    temp = Image.open("temp.jpg")
+    rgb_img_front = img_front.convert('RGB')
+    rgb_img_back = img_back.convert('RGB')
+
+    rgb_img_front.save("temp_front.jpg")
+    rgb_img_back.save("temp_back.jpg")
+
+    temp_front = Image.open("temp_front.jpg")
+    temp_back = Image.open("temp_back.jpg")
 
     # The jpeg file is converted to pdf.
-    pdf_bytes = img2pdf.convert("temp.jpg")
+    pdf_bytes = img2pdf.convert(["temp_front.jpg", "temp_back.jpg"])
 
     filename_pdf = 'output/pdf/' + id + '.pdf'
     os.makedirs(os.path.dirname(filename_pdf), exist_ok=True)
 
     file = open(filename_pdf, "wb")
     file.write(pdf_bytes)
-    os.remove("temp.jpg")
+    os.remove("temp_front.jpg")
+    os.remove("temp_back.jpg")
 
-    return {'card_image': filename_png, 'qr_image': filename_qr_png, 'pdf': filename_pdf}
+    return {'card_image': {'front': filename_front_png, 'back': filename_back_png}, 'qr_image': filename_qr_png, 'pdf': filename_pdf}
 
 if __name__ == '__main__':
     app.run()
