ad11d501426bb36c28231a6b94a3649ce71948c5
Tung <megatunger@gmail.com>
2020-09-12 16:46:50 +0700

    New API!

--- app.py
+++ app.py
@@ -1,58 +1,40 @@
-from flask import Flask, request, send_file, redirect, url_for, send_from_directory
-import sys
+from flask import Flask, request, send_file, redirect, url_for, send_from_directory, send_file
 import os
-import img2pdf
-import pyqrcode
 from PIL import Image, ImageOps, ImageFont, ImageDraw
 
 BASE_DIR = os.path.dirname(__file__)
 UPLOAD_FOLDER = 'output'
 
 app = Flask(__name__)
-@app.route('/')
-def get():
-    return 'POST request available only.'
 
-@app.route('/output/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    uploads = os.path.join(BASE_DIR, UPLOAD_FOLDER)
-    return send_from_directory(directory=uploads, filename=filename)
-
-
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET'])
 def main():
     id = request.args.get('id', ' ')
     name = request.args.get('name', ' ')
     school = request.args.get('school', ' ')
-    id_encrypt = request.args.get('id_encrypt', ' ')
-    return process(id, name, school, id_encrypt)
+    return send_file(process(id, name, school), as_attachment=True)
 
 @app.route('/', methods = ['DELETE'])
 def clearCache():
     id = request.args.get('id', ' ')
     try:
-        os.remove("output/png/" + id + ".png")
-        os.remove("output/qr/" + id + ".png")
-        os.remove("output/pdf/" + id + ".pdf")
+        os.remove("output/" + id + ".png")
         return "success"
     except:
         return "file not found."
 
-def process(id, name, school, id_encrypt):
+def process(id, name, school):
     # Set params
     string_limit = 21
     id = id[:string_limit]
     name = name[:string_limit]
     school = school[:string_limit]
-    id_encrypt = id_encrypt
     # opening image
     img_front = Image.open("Template/Front.png")
-    img_back = Image.open("Template/Back.png")
     draw = ImageDraw.Draw(img_front)
 
     # opening font files
-    font_regular = ImageFont.truetype("fonts/Montserrat-Regular.otf", 56)
-    font_bold = ImageFont.truetype("fonts/Montserrat-SemiBold.otf", 56)
+    font_bold = ImageFont.truetype("fonts/Rene Bieder - Gentona Bold.otf", 56)
 
     # Writing text at specific positions and specific color.
 
@@ -60,50 +42,13 @@ def process(id, name, school, id_encrypt):
     draw.text((193, 711), name, (22, 28, 42), font=font_bold)
     draw.text((193, 1010), school, (22, 28, 42), font=font_bold)
 
-    # Create qr image
-    qr = pyqrcode.create('{"login_token":"'+id_encrypt+'","otp": "offline_card"}')
+    # Save to disk
 
-    filename_qr_png = 'output/qr/' + id + '.png'
-    os.makedirs(os.path.dirname(filename_qr_png), exist_ok=True)
-
-    qr.png(filename_qr_png, scale=9)
-
-    # QR image is superimposed on the ID-card image with itself as a filter
-    qr_img = Image.open(filename_qr_png)
-    img_front.paste(qr_img, (1225, 355))
-
-    #Save to disk
-
-    filename_front_png = 'output/png/' + id + '_front.png'
-    filename_back_png = 'output/png/' + id + '_back.png'
+    filename_front_png = 'output/' + id + '.png'
     os.makedirs(os.path.dirname(filename_front_png), exist_ok=True)
-    os.makedirs(os.path.dirname(filename_back_png), exist_ok=True)
 
     img_front.save(filename_front_png)
-    img_back.save(filename_back_png)
-
-    # To convert to pdf the image is temporarily stored as jpeg
-    rgb_img_front = img_front.convert('RGB')
-    rgb_img_back = img_back.convert('RGB')
-
-    rgb_img_front.save("temp_front.jpg")
-    rgb_img_back.save("temp_back.jpg")
-
-    temp_front = Image.open("temp_front.jpg")
-    temp_back = Image.open("temp_back.jpg")
-
-    # The jpeg file is converted to pdf.
-    pdf_bytes = img2pdf.convert(["temp_front.jpg", "temp_back.jpg"])
-
-    filename_pdf = 'output/pdf/' + id + '.pdf'
-    os.makedirs(os.path.dirname(filename_pdf), exist_ok=True)
-
-    file = open(filename_pdf, "wb")
-    file.write(pdf_bytes)
-    os.remove("temp_front.jpg")
-    os.remove("temp_back.jpg")
-
-    return {'card_image': {'front': filename_front_png, 'back': filename_back_png}, 'qr_image': filename_qr_png, 'pdf': filename_pdf}
+    return filename_front_png
 
 if __name__ == '__main__':
     app.run()
